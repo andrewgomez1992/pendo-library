429ae56b6a27363446730848498cc83304564412
Andrew.Gomez <keepitgomez@yahoo.com>
2023-01-20 07:36:44 -0800

    updates name for testing

--- Pendo.js
+++ Pendo.js
@@ -3,8 +3,8 @@
 const apiKey = '8b61ff73-e191-4939-795c-c1f1817cc348';
 
 function generateUniqueIds() {
-  const visitorId = 'Mac Miller';
-  const accountId = 'Mac Miller';
+  const visitorId = 'Joe Rogan';
+  const accountId = 'Joe Rogan';
   return { visitorId, accountId };
 }
 
