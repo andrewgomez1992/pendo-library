e143e47e3598cd9507664cd800a44d51f90f7483
Andrew.Gomez <keepitgomez@yahoo.com>
2023-02-21 12:45:28 -0800

    latest

--- Pendo.js
+++ Pendo.js
@@ -3,8 +3,8 @@
 const apiKey = '8b61ff73-e191-4939-795c-c1f1817cc348';
 
 function generateUniqueIds() {
-  const visitorId = 'CodeBro JUNIOR';
-  const accountId = 'CodeBro JUNIOR';
+  const visitorId = 'Ares';
+  const accountId = 'Ares';
   return { visitorId, accountId };
 }
 
